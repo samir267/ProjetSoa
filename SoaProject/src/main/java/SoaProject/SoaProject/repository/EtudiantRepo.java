a582ab5eadde1326ae74163a0fa1e6716bdf6ed6
samir chemkhi <samirchemkhi09@gmail.com>
2023-10-22 15:35:21 +0100

    crud etudiant

---
+++ SoaProject/src/main/java/SoaProject/SoaProject/repository/EtudiantRepo.java
@@ -0,0 +1,10 @@
+package SoaProject.SoaProject.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import SoaProject.SoaProject.model.Etudiant;
+
+    public interface EtudiantRepo extends JpaRepository<Etudiant, Long> {
+
+}
+
---
+++ SoaProject/src/test/java/SoaProject/SoaProject/SoaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package SoaProject.SoaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
